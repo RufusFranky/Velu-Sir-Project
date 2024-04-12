84b6270edf76f72a143b1c7a7b988ad8b39543d6
RufusFranky <151731451+RufusFranky@users.noreply.github.com>
2024-04-12 19:17:49 +0530

    aligment

--- js/showcase.js
+++ js/showcase.js
@@ -49,3 +49,25 @@ addFunctionOnWindowLoad(fun2);
 addFunctionOnWindowLoad(fun3);
 
 console.log(5)
+
+// image scroll change handler
+
+var onScrollHandler = function() {
+    let myElem=document.getElementById('logo')
+  document.getElementById('nav-menu').style.height="95px"
+    var newImageUrl = myElem.src;
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if (scrollTop <200) {
+       newImageUrl = "../Images/logo.png"
+       myElem.style.height="140px"
+       myElem.style.transition=".5s"
+
+    }
+    if (scrollTop > 200) {
+       newImageUrl = "../Images/logo-rect.jpg"
+       myElem.style.height="90px"
+       myElem.style.transition=".5s"
+    }
+    myElem.src = newImageUrl;
+  };
+  document.addEventListener ("scroll", onScrollHandler);
