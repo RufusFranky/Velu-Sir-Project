8722b45e51a0b57ec392fd4a4cdc310d728497b0
RufusFranky <151731451+RufusFranky@users.noreply.github.com>
2024-04-30 11:58:58 +0530

    section updation

--- js/showcase.js
+++ js/showcase.js
@@ -3,11 +3,11 @@ function fun1() {
     
     var interval = setInterval(function () {
         count += 1;
-        if(count === 6){
+        if(count === 4){
             clearInterval(interval);
         }
         document.getElementById("view").innerHTML = count + "+";
-    },100);
+    },200);
 }
 
 function fun2() {
@@ -15,11 +15,11 @@ function fun2() {
     
     var interval = setInterval(function () {
         count += 1;
-        if(count === 25){
+        if(count === 20){
             clearInterval(interval);
         }
         document.getElementById("view1").innerHTML = count + "+";
-    },100);
+    },400);
 }
 
 function fun3() {
@@ -27,11 +27,11 @@ function fun3() {
     
     var interval = setInterval(function () {
         count += 1;
-        if(count === 50){
+        if(count === 500){
             clearInterval(interval);
         }
         document.getElementById("view2").innerHTML = count + "+";
-    },100);
+    },60);
 }
 
 var addFunctionOnWindowLoad = function (callback) {
@@ -95,4 +95,33 @@ var onScrollHandler = function() {
       }
     });
   });
+
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById("myImg");
+  var modalImg = document.getElementById("modal-img");
+  var captionText = document.getElementById("caption");
+  img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  
+  document.addEventListener("click", (e) => {
+    const elem = e.target;
+    if (elem.id === "myImg") {
+      modal.style.display = "block";
+      modalImg.src = elem.dataset.biggerSrc || elem.src;
+      captionText.innerHTML = elem.alt;
+    }
+  })
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
 
