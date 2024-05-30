4931ccf313363edbf2308266a2ca3d285a13ae3d
RufusFranky <151731451+RufusFranky@users.noreply.github.com>
2024-05-31 03:04:14 +0530

    alignment changes

--- js/showcase.js
+++ js/showcase.js
@@ -1,54 +1,3 @@
-// function fun1() {
-//   var count = 0;
-
-//   var interval = setInterval(function () {
-//     count += 1;
-//     if (count === 4) {
-//       clearInterval(interval);
-//     }
-//     document.getElementById("view").innerHTML = count + "+";
-//   }, 200);
-// }
-
-// function fun2() {
-//   var count = 0;
-
-//   var interval = setInterval(function () {
-//     count += 1;
-//     if (count === 20) {
-//       clearInterval(interval);
-//     }
-//     document.getElementById("view1").innerHTML = count + "+";
-//   }, 400);
-// }
-
-// function fun3() {
-//   var count = 0;
-
-//   var interval = setInterval(function () {
-//     count += 1;
-//     if (count === 500) {
-//       clearInterval(interval);
-//     }
-//     document.getElementById("view2").innerHTML = count + "+";
-//   }, 60);
-// }
-
-// var addFunctionOnWindowLoad = function (callback) {
-//   if (window.addEventListener) {
-//     window.addEventListener('load', callback, false);
-//   } else {
-//     window.attachEvent('onload', callback);
-//   }
-// }
-
-// addFunctionOnWindowLoad(fun1);
-
-// addFunctionOnWindowLoad(fun2);
-
-// addFunctionOnWindowLoad(fun3);
-
-// image scroll change handler
 
 var onScrollHandler = function () {
   let myElem = document.getElementById('logo')
@@ -56,12 +5,12 @@ var onScrollHandler = function () {
   var newImageUrl = myElem.src;
   var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
   if (scrollTop < 200) {
-    newImageUrl = "../Images/logo.png"
+    newImageUrl = "../Images/logo.webp"
     myElem.style.height = "140px"
     myElem.style.transition = ".5s ease-out"
   }
   if (scrollTop > 200) {
-    newImageUrl = "../Images/logo.png"
+    newImageUrl = "../Images/logo.webp"
     myElem.style.height = "104px"
     myElem.style.transition = ".5s ease-out"
 
@@ -69,7 +18,6 @@ var onScrollHandler = function () {
   myElem.src = newImageUrl;
 };
 document.addEventListener("scroll", onScrollHandler);
-
 
 //actvie nav
 
@@ -92,44 +40,3 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
-
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-// var img = document.getElementById("myImg");
-// var modalImg = document.getElementById("modal-img");
-// var captionText = document.getElementById("caption");
-// img.onclick = function () {
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-//   captionText.innerHTML = this.alt;
-// }
-
-// document.addEventListener("click", (e) => {
-//   console.log(e.target.name);
-//   var modal = document.getElementById("myModal");
-//   if (e.target.classList[0] == "close") {
-//     modal.style.display = "none";
-//   } else {
-//     document.getElementById('Play-Grp').style.display = "none"
-//     document.getElementById('Pre-Kg').style.display = "none"
-//     document.getElementById('Jr-Kg').style.display = "none"
-//     document.getElementById('Sr-Kg').style.display = "none"
-//     const elem = e.target;
-//     document.getElementById("myModal").style.display = "block"
-//     document.getElementById(elem.name).style.display = "block"
-//   }
-//   if (elem.name === "myImg") {
-//     modal.style.display = "block";
-//     modalImg.src = elem.dataset.biggerSrc || elem.src;
-//     captionText.innerHTML = elem.alt;
-//   }
-
-// })
-
-// Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function () {
-//   modal.style.display = "none";
-// }
